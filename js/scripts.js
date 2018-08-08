0dae0029a6afcd83f64b743dd7865e1529902a25
Winston <wagawc@gmail.com>
2018-08-08 10:16:47 +0300

    add title, link html with scripts.js, add head element then determine grade bars

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var marks = parseInt(prompt("Enter marks"));
+if (marks <=100 && marks >=80) {
+  alert("Grade A");
+}
+else if (marks <=79 && marks >=60) {
+  alert ("Grade B");
+}
+else if (marks <=59 && marks >=40) {
+  alert ("Grade C");
+}
+else if (marks <=39 && marks >=20) {
+  alert("Grade D");
+}
+else if (marks <=20 && marks >=0) {
+  alert("Grade E")
+}
